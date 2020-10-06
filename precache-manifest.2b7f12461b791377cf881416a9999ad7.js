537a62192b72864f224c5c711911a6dd79f46285
Mitchell Patterson <mpatterson12@live.ca>
2020-10-06 08:47:09 -0400

    Updates

---
+++ precache-manifest.2b7f12461b791377cf881416a9999ad7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfca4ed90bd7c4037282f98f1ee8cc05",
+    "url": "/spreadsMadeSimple/index.html"
+  },
+  {
+    "revision": "6300f1ca147b4b1d2f3c",
+    "url": "/spreadsMadeSimple/static/css/main.c941dec0.chunk.css"
+  },
+  {
+    "revision": "2c2616560309c3b972cd",
+    "url": "/spreadsMadeSimple/static/js/2.38a8f1e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spreadsMadeSimple/static/js/2.38a8f1e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6300f1ca147b4b1d2f3c",
+    "url": "/spreadsMadeSimple/static/js/main.bc64441c.chunk.js"
+  },
+  {
+    "revision": "6f7048679cdabd18d194",
+    "url": "/spreadsMadeSimple/static/js/runtime-main.267d7f0a.js"
+  },
+  {
+    "revision": "de28072afd0a761ccb45c040cbfffd18",
+    "url": "/spreadsMadeSimple/static/media/function.de28072a.svg"
+  },
+  {
+    "revision": "80b1a67f3253c6fb0c518109d8a0943b",
+    "url": "/spreadsMadeSimple/static/media/minus.80b1a67f.svg"
+  },
+  {
+    "revision": "497f062c699cd9130fd77bc7704d82ef",
+    "url": "/spreadsMadeSimple/static/media/plus.497f062c.svg"
+  },
+  {
+    "revision": "077efaf595bc8b1140df3dacbc599185",
+    "url": "/spreadsMadeSimple/static/media/spreadsheet.077efaf5.svg"
+  }
+]);
